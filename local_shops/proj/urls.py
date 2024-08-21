24c3cdf0dd956ef31ac0ce25a579b99ae030d399
Ayanguradi <ayanguradi2@gmail.com>
2024-08-21 17:33:57 +0530

    error2
    req file  changed

--- local_shops/proj/urls.py
+++ local_shops/proj/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 
 # local_shops/urls.py
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path, include
